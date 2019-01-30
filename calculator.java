9648d3afda10d2817c3eee95421c74a662c4bc4d
Rashid9158 <42002102+Rashid9158@users.noreply.github.com>
2019-01-30 19:40:14 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -32,12 +32,12 @@ import java.util.Scanner;
 	                 System.out.print("Result = " + res);
 	                 break;
 	 
-	      /*case '3' : System.out.print("Enter Two Number : ");
+	      case '3' : System.out.print("Enter Two Number : ");
 	                 a = scan.nextFloat();
 	                 b = scan.nextFloat();
 	                 res = a * b;
 	                 System.out.print("Result = " + res);
-	                 break;*/
+	                 break;
 	 
 	      case '4' : System.out.print("Enter Two Number : ");
 	                 a = scan.nextFloat();
